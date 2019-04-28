30e739ab2cf76b84264f5ac4f68e8f7e80e64aee
Jermaine Gyekye <jgyekye@gmail.com>
2019-04-28 15:51:24 +0100

    links

--- wp/wp-content/themes/blankslate-child/template-parts/default-main.php
+++ wp/wp-content/themes/blankslate-child/template-parts/default-main.php
@@ -1,12 +1,16 @@
-<header id="default-page-title">
+<? if (get_header_image()) { ?>
+  <header id='default-page-title' style='background-image: url("<? echo get_header_image() ?>")'>
+<? } else { ?>
+  <header id="default-page-title">
+<? } ?>
   <div class="wrapper">
     <h1><? the_title(); ?></h1>  
   </div>
 </header>
 <article id="default-page-main">
-  <?php if (has_post_thumbnail()) : ?>
+  <!-- <?php if (has_post_thumbnail()) : ?>
     <? get_template_part('template-parts/default', 'featured-image'); ?>
-  <? endif; ?>
+  <? endif; ?> -->
     <? the_content(); ?>
 
     <?php if (is_active_sidebar('custom-side-bar')) : ?>
