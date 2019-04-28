69a2ed2aacceed816c1e93710db3f35c0b31d949
Jermaine Gyekye <jgyekye@gmail.com>
2019-04-28 20:16:56 +0100

    menu js

--- htmlpages/javascript.js
+++ htmlpages/javascript.js
@@ -1,10 +1,13 @@
 const mobileMenuBtn = document.querySelector('.burger-btn-js');
 const menu = document.getElementById('site-title');
 
-mobileMenuBtn.addEventListener('click', () => {
-  if (menu.classList.contains('open')) {
-    menu.classList.remove('open');
-  } else {
-    menu.classList.add('open');
-  }
-});
+if (mobileMenuBtn) {
+  mobileMenuBtn.addEventListener('click', () => {
+    if (menu.classList.contains('open')) {
+      menu.classList.remove('open');
+    } else {
+      menu.classList.add('open');
+    }
+  });
+};
+
